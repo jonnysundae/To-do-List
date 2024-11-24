b420c7dcc3d1174b836bbba711d78194ca48f994
jonnysundae <contatojonathannascimento@gmail.com>
2024-11-23 23:27:47 -0300

    Entity construído

---
+++ todolist/src/test/java/com/oisoujon/projetos/todolist/entity/Todo.java
@@ -0,0 +1,57 @@
+package com.oisoujon.projetos.todolist.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+// Informa que é uma entidade
+@Table(name = "todos")
+//Cria uma tabela "ToDos" no banco de dados
+public class Todo {
+    @Id
+    //Informa que a próxima variável é uma ID
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //Gera o valor de ID seguindo uma estratégia de geração
+    private Long id;
+    private String nome;
+    private String descricao;
+    private boolean realizado;
+    private int prioridade;
+
+    //Gerar os Getters e Setters pelo caminho: Botão Direito -> Source Action
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public boolean isRealizado() {
+        return realizado;
+    }
+    public void setRealizado(boolean realizado) {
+        this.realizado = realizado;
+    }
+    public int getPrioridade() {
+        return prioridade;
+    }
+    public void setPrioridade(int prioridade) {
+        this.prioridade = prioridade;
+    }
+
+    
+}
